b9310ba35ab0d6b4d1cba84dcaf47d560cf8bba0
VyomSachan <67855656+VyomSachan@users.noreply.github.com>
2023-06-04 20:23:05 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    console.log('Ready')
+
+    // Fetch the current date and update it in the DOM
+    let date = new Date()
+    let display_date = date.toLocaleDateString()
+
+    $(document).ready(function(){
+        $("#display_date").html(display_date)
+    })
+
+    let predicted_emotion
+
+    $(function () {
+        $("#predict_button").click(function () {
+            //  get the text value from the textarea using the 'val()' method
+            let text_value = $('').val()
+
+            //  Convert it to JS object.
+            //  Provide a 'key' here and in write the same in app.py file as well to extract data
+            let input_text = {'' : text_value}
+            console.log(input_text)
+
+            //  ajax request
+            $.ajax({
+                //  type of web request
+                type : "POST",
+                url : "/predict",
+    
+                //  Data to be sent in JSON format
+                data : JSON.stringify(),
+    
+                //  type of response expected is json
+                dataType : 'json',
+    
+                //  contentType
+                contentType : 'application/json',
+    
+                //  if everything is successful, run this function
+                success : function(result){
+                    $('#sentiment').text(prediction)
+                    $('#sentiment').show()
+
+                    $('#emoji').attr('src' , emoji_url)
+                    $('#emoji').show()
+                },
+    
+                //Error function
+                error : function(result){
+                    alert(result.responseJSON.message)
+                }
+            });
+        });
+    });
+});
